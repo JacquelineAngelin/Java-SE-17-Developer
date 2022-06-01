edd380aaef65918b0f286f2c4e25d50bf1e635c5
Jacqueline Angelin <jacqueline.silva@sensedia.com>
2022-06-01 16:35:43 -0300

    Overloading methods

---
+++ src/fundamentals/methods/MainFundamentals.java
@@ -0,0 +1,26 @@
+package fundamentals.methods;
+
+public class MainFundamentals {
+
+    public static void main (String[] args){
+
+        //Calculor
+        System.out.println("Caculator");
+        Calculator.soma(3, 6);
+        Calculator.subtracao(9, 1.8);
+        Calculator.multiplicacao(7, 8);
+        Calculator.divisao(5, 2.5);
+
+        //Message
+        System.out.println("Message");
+        Message.findMessage(9);
+        Message.findMessage(17);
+        Message.findMessage(21);
+
+        //Financial
+        System.out.println("Financial-Financiamento");
+        Financial.calculatorValue(1000, Financial.getDuasParcelas());
+        Financial.calculatorValue(1000, Financial.getTresParcelas());
+        Financial.calculatorValue(1000, 5);
+    }
+}
---
+++ src/overloading/methods/MainOverloading.java
@@ -0,0 +1,14 @@
+package overloading.methods;
+
+public class MainOverloading {
+
+    public static void main (String[] args){
+
+//        //Tetragon
+        System.out.println("Quadrilatero");
+        Tetragon.area(3);
+        Tetragon.area(5, 5);
+        Tetragon.area(7, 3, 2);
+        Tetragon.area(5, 5);
+    }
+}
