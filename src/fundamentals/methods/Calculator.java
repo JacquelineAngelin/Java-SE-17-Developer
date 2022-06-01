2788b321d37ff0267b1971b2e56896abbc2227a2
Jacqueline Angelin <jacqueline.silva@sensedia.com>
2022-06-01 11:48:13 -0300

    adjust calculator

--- src/fundamentals/methods/Calculator.java
+++ src/fundamentals/methods/Calculator.java
@@ -4,21 +4,21 @@ public class Calculator {
 
     public static void soma(double numero1, double numero2){
         double result = numero1 + numero2;
-        System.out.println("A soma de "+ numero1 + "+" + numero2 + "resulta em: "+ result);
+        System.out.println("A soma de "+ numero1 + " + " + numero2 + " resulta em: "+ result);
     }
 
     public static void subtracao(double numero1, double numero2){
         double result = numero1 - numero2;
-        System.out.println("A subtração de "+ numero1 + "-" + numero2 + "resulta em: "+ result);
+        System.out.println("A subtração de "+ numero1 + " - " + numero2 + " resulta em: "+ result);
     }
 
     public static void multiplicacao(double numero1, double numero2){
         double result = numero1 * numero2;
-        System.out.println("A multiplicação de "+ numero1 + "x" + numero2 + "resulta em: "+ result);
+        System.out.println("A multiplicação de "+ numero1 + " x " + numero2 + " resulta em: "+ result);
     }
 
     public static void divisao(double numero1, double numero2){
         double result = numero1 / numero2;
-        System.out.println("A divisão de "+ numero1 + "/" + numero2 + "resulta em: "+ result);
+        System.out.println("A divisão de "+ numero1 + " / " + numero2 + " resulta em: "+ result);
     }
 }
