65b14e80f1257228244eb9be94581f6f802f3569
Jacqueline Angelin <jacqueline.silva@sensedia.com>
2022-06-01 11:45:16 -0300

    update calculator

--- src/fundamentals/methods/Main.java
+++ src/fundamentals/methods/Main.java
@@ -1,4 +1,20 @@
 package fundamentals.methods;
 
 public class Main {
+
+    public static void main (String[] args){
+
+        //Calculor
+        System.out.println("Caculator");
+        Calculator.soma(3, 6);
+        Calculator.subtracao(9, 1.8);
+        Calculator.multiplicacao(7, 8);
+        Calculator.divisao(5, 2.5);
+
+        //Message
+//        System.out.println("Message");
+//        Message.findMessage(9);
+//        Message.findMessage(14);
+//        Message.findMessage(1);
+    }
 }
