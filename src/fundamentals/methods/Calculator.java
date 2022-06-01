121daf91beb5e89bf199f03aedfe20a00a487b4d
Jacqueline Angelin <jacqueline.silva@sensedia.com>
2022-06-01 11:29:50 -0300

    update methods

--- src/fundamentals/methods/Calculator.java
+++ src/fundamentals/methods/Calculator.java
@@ -1,4 +1,24 @@
 package fundamentals.methods;
 
 public class Calculator {
+
+    public static void soma(double numero1, double numero2){
+        double resultado = numero1 + numero2;
+        System.out.println("A soma de "+ numero1 + "+" + numero2 + "resulta em: "+ resultado);
+    }
+
+    public static void subtracao(double numero1, double numero2){
+        double resultado = numero1 - numero2;
+        System.out.println("A subtração de "+ numero1 + "-" + numero2 + "resulta em: "+ resultado);
+    }
+
+    public static void multiplicacao(double numero1, double numero2){
+        double resultado = numero1 * numero2;
+        System.out.println("A multiplicação de "+ numero1 + "x" + numero2 + "resulta em: "+ resultado);
+    }
+
+    public static void divisao(double numero1, double numero2){
+        double resultado = numero1 / numero2;
+        System.out.println("A divisão de "+ numero1 + "/" + numero2 + "resulta em: "+ resultado);
+    }
 }
