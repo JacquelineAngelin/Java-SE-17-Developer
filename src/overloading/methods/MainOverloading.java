a703f82ec01ca62a20f7251b2b9845a9ba2dc822
Jacqueline Angelin <jacqueline.silva@sensedia.com>
2022-06-01 16:54:24 -0300

    diff with type for retangulo and losango

--- src/overloading/methods/MainOverloading.java
+++ src/overloading/methods/MainOverloading.java
@@ -7,8 +7,8 @@ public class MainOverloading {
 //        //Tetragon
         System.out.println("Quadrilatero");
         Tetragon.area(3);
-        Tetragon.area(5, 5);
+        Tetragon.area(5d, 5d);
         Tetragon.area(7, 3, 2);
-        Tetragon.area(5, 5);
+        Tetragon.area(5f, 5f);
     }
 }
--- src/overloading/methods/Tetragon.java
+++ src/overloading/methods/Tetragon.java
@@ -3,10 +3,12 @@ package overloading.methods;
 public class Tetragon {
 
     public static void area (double lado){
+
         System.out.println("Area do quadrado: " + lado * lado);
     }
 
     public static void area (double lado1, double lado2){
+
         System.out.println("Area do retangulo: " + lado1 * lado2);
     }
 
