3468a659ce67c13d0cb23c41cd607a5cff3dd619
Jacqueline Angelin <jacqueline.silva@sensedia.com>
2022-06-06 12:22:01 -0300

    refactor tratragonUpdate

--- src/overload/methods/upgrade/TetragonUpgrade.java
+++ src/overload/methods/upgrade/TetragonUpgrade.java
@@ -4,21 +4,17 @@ public class TetragonUpgrade {
 
     public static double area (double lado){
         return (lado * lado);
-        //System.out.println("Area do quadrado: " + lado * lado);
     }
 
     public static double area (double lado1, double lado2){
         return (lado1 * lado2);
-        //System.out.println("Area do retangulo: " + lado1 * lado2);
     }
 
     public static double area (double baseMaior, double baseMenor, double altura){
         return ((baseMaior+baseMenor)*altura)/2;
-        //System.out.println("Area do trapezio: " + ((baseMaior+baseMenor)*altura)/2);
     }
 
     public static float area (float diagonal1, float diagonal2){
         return (diagonal1*diagonal2)/2;
-        //System.out.println("Area do losango: " + (diagonal1*diagonal2)/2);
     }
 }
