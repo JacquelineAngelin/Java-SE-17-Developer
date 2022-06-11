f560f63a6143ca00000000cbb0017f9cf504afeb
Jacqueline Angelin <jacqueline.silva@sensedia.com>
2022-06-10 23:59:14 -0300

    nano automate reader

--- src/repeater/auto/Aluno.java
+++ src/repeater/auto/Aluno.java
@@ -1,45 +1,45 @@
-package repeater.auto;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-
-public class Aluno {
-
-    ArrayList aluno = new ArrayList();
-
-    File caminhoDoTexto = new File("C:\\Users\\jacqueline.silva\\Projetos\\estudos");
-
-    FileReader lerCaminhoDoTexto;
-
-    {
-        try {
-            lerCaminhoDoTexto = new FileReader(caminhoDoTexto, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    BufferedReader lerArquivo = new BufferedReader(lerCaminhoDoTexto);
-
-    while(lerArquivo.read())
-
-    {
-        String texto = null;
-        try {
-            texto = lerArquivo.readLine();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        String[] textoSplit = texto.split(";");
-
-        for (int posicao = 0; posicao < 4; posicao++) {
-            dadosAluno.aluno.add(textoSplit[posicao]);
-            System.out.print(textoSplit[1]);
-        }
-    }
-    lerArquivo.close();
-}
+//package repeater.auto;
+//
+//import java.io.BufferedReader;
+//import java.io.File;
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.nio.charset.StandardCharsets;
+//import java.util.ArrayList;
+//
+//public class Aluno {
+//
+//    ArrayList aluno = new ArrayList();
+//
+//    File caminhoDoTexto = new File("C:\\Users\\jacqueline.silva\\Projetos\\estudos");
+//
+//    FileReader lerCaminhoDoTexto;
+//
+//    {
+//        try {
+//            lerCaminhoDoTexto = new FileReader(caminhoDoTexto, StandardCharsets.UTF_8);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    BufferedReader lerArquivo = new BufferedReader(lerCaminhoDoTexto);
+//
+//    while(lerArquivo.read())
+//
+//    {
+//        String texto = null;
+//        try {
+//            texto = lerArquivo.readLine();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        String[] textoSplit = texto.split(";");
+//
+//        for (int posicao = 0; posicao < 4; posicao++) {
+//            dadosAluno.aluno.add(textoSplit[posicao]);
+//            System.out.print(textoSplit[1]);
+//        }
+//    }
+//    lerArquivo.close();
+//}
---
+++ src/repeater/auto/NanoAutomateReader.java
@@ -0,0 +1,25 @@
+package repeater.auto;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class NanoAutomateReader {
+
+    public static void main (String[] args){
+
+        String path = "C:\\Users\\jacqueline.silva\\Projetos\\estudos\\alunos.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
+            String line = br.readLine();
+            while (line != null){
+                System.out.println(line + "complemento do texto");
+                System.out.println("*." + line + "complemento do texto");
+                line = br.readLine();
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
